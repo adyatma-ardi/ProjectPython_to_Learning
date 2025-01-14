74e48abf1b354c35ea2f66f5dad2c2a9244bddd2
adyatma-ardi <adyatmaardi@gmail.com>
2025-01-14 23:25:59 +0700

    band_name_generator.py complete

---
+++ beginner/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Selamat datang di program penghasil nama band!")
+city = input("kamu tinggal dimana ?\n")
+pet = input("Kamu punya hewan peliharaan apa ?\n")
+print("Nama bandmu adalah " + city + " " + pet)
