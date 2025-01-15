7a76133a0afe9f1ae25fc79122ce10df43b8557a
adyatma-ardi <adyatmaardi@gmail.com>
2025-01-15 16:16:03 +0700

    mathematical operators

---
+++ src/mathematical_operators.py
@@ -0,0 +1,14 @@
+print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 + 2)
+print(5 / 3)
+print(5 // 3)
+print(2**4)
+
+# PEMDAS (Parentheses, Exponents, Multiplication/Division, Addition/Subtraction)
+
+print(3 * 3 + 3 / 3 - 3)
+
+# output 3 ?
+print(((3 * (3 + 3)) / 3)- 3)
