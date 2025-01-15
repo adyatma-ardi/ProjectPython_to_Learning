190e9b3dbae4151b686fcf4578a9659b8c6c350e
adyatma-ardi <adyatmaardi@gmail.com>
2025-01-15 15:58:05 +0700

    type error, type checking and type conversion

---
+++ src/typeerror_typechecking_typeconversion.py
@@ -0,0 +1,19 @@
+print(len("Hello"))
+
+print(type(123))
+
+print(type(int("123")))
+
+print(int("312")+int("10"))
+
+int()
+float()
+str()
+bool()
+
+# print("Number of letters in your name: " + len(input("Enter your name")))
+
+username_input = input("Enter your name")
+length_username = len(username_input)
+
+print("Number of letter in your name : " + str(length_username))
