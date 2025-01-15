5b690076d3bd6418e3521908f66bba6575a2b5e8
adyatma-ardi <adyatmaardi@gmail.com>
2025-01-15 15:11:48 +0700

    primitive data types

---
+++ src/primitive_data_types.py
@@ -0,0 +1,21 @@
+# primitive data types
+
+# subscription
+print("Achmad"[3])
+print("Achmad"[-2])
+
+# string
+print("123" + "345")
+
+# integere / whole number
+print(123 + 345)
+
+# large integer
+print(123_345 + 213)
+
+# floating number
+print(3145.49  + 0.2)
+
+# boolean
+print(True)
+print(False)
